a209a082147277e1145900a27e6dce2672ff8223
johnwmillr <john.w.millr@gmail.com>
2017-10-27 09:07:27 -0500

    Clean up directions.rb and geocoding.rb

--- google-maps-api/directions.rb
+++ google-maps-api/directions.rb
@@ -5,11 +5,11 @@ file_lines = []
 File.open('../secrets.txt','r') do |f|
     f.each_line {|line| file_lines = file_lines + [line.split(':')[1].strip()]}
 end
-api_key_directions = file_lines[1]
+api_key = file_lines[1]
+raise "Missing API key." unless api_key.is_a? String
 
 # Initialize the Directions API
-gmaps_directions = GoogleMapsService::Client.new(key: api_key_directions)
+gmaps = GoogleMapsService::Client.new(key: api_key)
 routes = gmaps.directions('1700 S. Main Street, Goshen, IN', '1631 S. Main Street, Goshen, IN')
 
-puts '********************'
 puts routes[0].inspect
--- google-maps-api/geocoding.rb
+++ google-maps-api/geocoding.rb
@@ -5,18 +5,17 @@ file_lines = []
 File.open('../secrets.txt','r') do |f|
     f.each_line {|line| file_lines = file_lines + [line.split(':')[1].strip()]}
 end
-api_key_geocoding = file_lines[0]
+api_key = file_lines[0]
 
 # Initialize the Geocoding API
-gmaps_geocoding = GoogleMapsService::Client.new(key: api_key_geocoding)
+gmaps = GoogleMapsService::Client.new(key: api_key)
 
 # Do some reverse geocoding
-latlong = [41.6609926,-91.5359006] # Pentacrest
-latlong = [41.6596182,-91.5369034] # Seamans Center
-latlong = [41.660214,-91.5343671] # Pancheros
-results = gmaps.reverse_geocode(latlng)
+# latlong = [41.6609926,-91.5359006] # Pentacrest
+# latlong = [41.6596182,-91.5369034] # Seamans Center
+latlong = [41.660214,-91.5343671]  # Pancheros
+results = gmaps.reverse_geocode(latlong)
 
-puts '********************'
 results.each do |r|
     puts '-----------'
     puts r[:formatted_address].inspect
