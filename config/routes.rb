5cc1fbecba2272177c8b979ce16dd4cfd62de472
Qingyang Su <qingyang-su@uiowa.edu>
2017-11-01 15:59:37 +0000

    add a barbone for login model

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+  resources :users,:merchants
+  match '/signup', to: 'users#new', via: :get
+  match '/login',  to: 'sessions#new', via: :get
+  match '/logout', to: 'sessions#destroy', via: :delete
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
