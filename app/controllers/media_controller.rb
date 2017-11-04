66dbeee72f33eb37448294d48ff1bcc9c13a2abf
johnwmillr <john.w.millr@gmail.com>
2017-11-04 14:26:03 -0500

    Add media table with example routes

---
+++ app/controllers/media_controller.rb
@@ -0,0 +1,11 @@
+class MediaController < ApplicationController
+
+
+
+
+def new
+  # render the views/media/new.html.haml template
+end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'users#index'
-  resources :users, :media
+  # root 'users#index'
+  resources :users do
+    resources :media
+  end
+  root :to => redirect('/users')
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
