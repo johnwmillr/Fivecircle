0d85975bc04d26faba0363f2c5101c1c72763752
johnwmillr <john.w.millr@gmail.com>
2017-11-04 11:46:45 -0500

    Add Media table to User view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   root 'users#index'
-  resources :users
+  resources :users, :media
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
