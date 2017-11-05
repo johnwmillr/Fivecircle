6ad94d466d6c24d13a7fcb194fef3e20f6640c1a
Qingyang Su <qingyang-su@uiowa.edu>
2017-11-05 20:42:16 +0000

    add authenticate user log in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,9 +7,17 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    user = User.find_by_username(params[:user][:username])
+    if user && user.valid_password?(params[:user][:password])
+      #sign in and redirect to show page
+      super
+    else
+      flash.now[:warning] = 'Invalid email/password combination'
+      render 'new'
+    end  
+    
+  end
 
   # DELETE /resource/sign_out
   # def destroy
--- app/models/user/parameter_sanitizer.rb
+++ app/models/user/parameter_sanitizer.rb
@@ -2,5 +2,6 @@ class User::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
     permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+    permit(:sign_in, keys: [:username, :password])
   end
 end
