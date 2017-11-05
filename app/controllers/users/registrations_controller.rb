3003a07102419af4bfb4ee134dd0d70fe25b835d
Qingyang Su <qingyang-su@uiowa.edu>
2017-11-05 16:57:37 +0000

    delete unnecessary part

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,9 +8,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
-  end
   
   def create
     # params["user"]["created_at"] = Time.now.strftime("%d/%m/%Y %H:%M")
