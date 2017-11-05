b7bd04b9113cad7f647832ba31aea4301dda0a63
johnwmillr <john.w.millr@gmail.com>
2017-11-05 17:52:27 -0600

    Add else option to /users/show.html/haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
     def show
-        @user = current_user
+        if current_user
+            @user = current_user
+        else
+            redirect_to root_path
+        end
     end
 
 end
