f87bc60210d22c975533af04293b425c66662401
Aman Gupta <aman-gupta@uiowa.edu>
2017-11-07 06:06:12 +0000

    Implemented the omniauth method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,5 +31,16 @@ class User < ActiveRecord::Base
   # end
   # after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
   
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 
 end
