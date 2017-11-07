92afb18b27bdda5bbe172fd7849248fb6a0fbea9
Aman Gupta <aman-gupta@uiowa.edu>
2017-11-07 06:36:12 +0000

    Method for user with new session

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,5 +42,13 @@ class User < ActiveRecord::Base
       # user.skip_confirmation!
     end
   end
+  
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 
 end
