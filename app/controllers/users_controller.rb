6f6c2e96b8a5bdb12ffdc9c932d6ff9207ad9456
Ahmed Elmaleh <ahmed-elmaleh@uiowa.edu>
2017-12-03 21:04:48 +0000

    still working on checkin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+    def user_params
+        params.require(:user).permit(:email, :lastAddress, :latitude, :longitude)
+    end
     def show
         if current_user
             @user = current_user
@@ -7,5 +9,7 @@ class UsersController < ApplicationController
             redirect_to root_path
         end
     end
-
+    
+    def add_location
+    end
 end
