7c7fceabd02ba42a8684ab912b1ff411d74ec52f
harrison white <harrison-white@uiowa.edu>
2017-12-08 05:50:01 +0000

    img path for heroku

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -10,4 +10,5 @@ Paperclip::Attachment.default_options[:s3_credentials] = {
 Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
 Paperclip::Attachment.default_options[:path] = "/:class/:attachment/:id_partition/:style/:hash.:extension"
 
-Paperclip.options[:command_path] = 'C:\ImageMagicks'
+# Paperclip.options[:command_path] = 'C:\ImageMagicks'
+Paperclip.options.merge!(:command_path => "/usr/bin")
